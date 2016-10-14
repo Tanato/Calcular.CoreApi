2d90b748710f541205353d930f0f85634a7f7f48
Tanato Cartaxo <tanato@ciandt.com>
2016-10-14 13:30:35 -0300

    Added test controller to test api load

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Calcular.CoreApi.Controllers
 {
